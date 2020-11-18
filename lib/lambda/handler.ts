8487efc5c5366814381b579fffbcf8c41fb027ae
Lucas Machado <lucasam@amazon.com>
2020-11-18 16:16:30 -0300

    Pipeline will NOT reach prod

--- lib/lambda/handler.ts
+++ lib/lambda/handler.ts
@@ -2,7 +2,7 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda
 
 export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
   return {
-    body: 'This will reach prod',
-    statusCode: 200,
+    body: 'This will NOT reach prod',
+    statusCode: 400,
   };
 }
