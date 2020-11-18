bf7abfd862bb608321d889a882450b7202078cc5
Lucas Machado <lucasam@amazon.com>
2020-11-18 11:56:41 -0300

    Initial deployment

--- bin/cdkpipelines-demo.ts
+++ bin/cdkpipelines-demo.ts
@@ -1,7 +1,13 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { CdkpipelinesDemoStack } from '../lib/cdkpipelines-demo-stack';
+import {CdkpipelinesDemoStack} from '../lib/cdkpipelines-demo-stack';
 
 const app = new cdk.App();
-new CdkpipelinesDemoStack(app, 'CdkpipelinesDemoStack');
+new CdkpipelinesDemoStack(app, 'CdkpipelinesDemoStack',
+    {
+      env: {account: '468454216279', region: 'us-east-1'},
+    });
+
+
+app.synth();
