1c5250d3c8747f107d020d8fc36b955adf980d76
Lucas Machado <lucasam@amazon.com>
2020-11-24 16:07:30 -0300

    Go ari

--- lib/lambda/handler.ts
+++ lib/lambda/handler.ts
@@ -2,7 +2,7 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda
 
 export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
   return {
-    body: 'This will SUPER reach prod',
+    body: 'Ari will survive',
     statusCode: 200,
   };
 }
